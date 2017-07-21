a638bd5fdd140cc7559542744f9058dc34a28977
Sidharth Nambiar <snambiar01@gmail.com>
2017-07-20 22:19:35 -0400

    started chapter on Files in Ruby

---
+++ backend_prep/learn_to_program_by_chris_pine/chapter_2/calc.rb
@@ -0,0 +1,5 @@
+# Learn To Program
+# Chapter 2: Numbers
+# calc.rb 
+
+puts 1+2
---
+++ backend_prep/learn_to_program_by_chris_pine/chapter_2/ex_1.rb
@@ -0,0 +1,23 @@
+# Learn To Program
+# Chapter 2: Numbers
+# Exercise 1
+
+# Write a program that tells you the following:
+
+# 1. Hours in a year. How many hours are in a year?
+
+  hours_in_a_year = 365 * 24
+  puts "There are #{hours_in_a_year} hours in a year."
+
+# 2. Minutes in a decade. Hoy many minutes are in a decade?
+  
+ 
+  minutes_in_a_year = 60 * hours_in_a_year
+  minutes_in_a_decade = 10*minutes_in_a_year
+   puts "There are #{minutes_in_a_decade} minutes in a decade."
+# 3. You age in seconds. How many seconds old are you?
+# 4. Our dear author's age. If I am 1,160 million seconds old, how old am I?
+
+# 365 days x 24 hours
+
+
