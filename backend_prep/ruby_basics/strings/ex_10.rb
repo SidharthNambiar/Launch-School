b977ef38ccf1131dec95f9be9b22ba7e303a6153
Sidharth Nambiar <snambiar01@gmail.com>
2017-08-06 13:28:32 -0400

    answered extra question in exercise 10

--- backend_prep/ruby_basics/strings/ex_10.rb
+++ backend_prep/ruby_basics/strings/ex_10.rb
@@ -11,3 +11,18 @@ colors = 'blue pink yellow orange'
 
 puts colors.include?("yellow")
 puts colors.include?("purple")
+
+# Further Exploration
+
+# Let's say color is changed to the following value:
+
+colors = 'blue boredom yellow'
+
+# and we invoke #include? as we did before:
+
+puts colors.include?('red')
+
+# What will the output be? Why?
+# Asnwer: The output will be true because the string colors contains the characters 'red' within it (inside boredom).
+# Your'e better off compartmentalizing the contents of the string into elements of an array as such:
+puts colors.split(" ").include?('red')
