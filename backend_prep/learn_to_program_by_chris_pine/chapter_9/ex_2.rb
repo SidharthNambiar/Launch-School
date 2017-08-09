2ddcafe8c9a513aaa26956894212174cbefcf598
Sidharth Nambiar <snambiar01@gmail.com>
2017-08-09 19:13:25 -0400

    completed chapter 9 exercises

--- backend_prep/learn_to_program_by_chris_pine/chapter_9/ex_2.rb
+++ backend_prep/learn_to_program_by_chris_pine/chapter_9/ex_2.rb
@@ -2,10 +2,26 @@
 # Chapter 9: Writing Your Own Methods
 # Exercise 2
 
-# Old-school Roman numerals.
+# Old-school Roman numerals (9 = VIIII,not IX)
+
+# I = 1     V = 5     X = 10   L = 50
+# C = 100   D = 500   M = 1000
+
 
 def old_roman_numeral(num)
+  
+ roman = ''
+
+ roman = roman << 'M' * (num/1000)
+ roman = roman << 'D' * (num%1000 /500)
+ roman = roman << 'C' * (num%500  /100)
+ roman = roman << 'L' * (num%100  /50)
+ roman = roman << 'X' * (num%50   /10)
+ roman = roman << 'V' * (num%10   /5)
+ roman = roman << 'I' * (num%5)
+
 
 end
-
-old_roman_numeral(4)
+puts "Please enter a number between 1 and 3000."
+number = gets.chomp.to_i
+puts old_roman_numeral(number)
---
+++ backend_prep/learn_to_program_by_chris_pine/chapter_9/ex_3.rb
@@ -0,0 +1,63 @@
+# Learn To Program
+# Chapter 9: Writing Your Own Methods
+# Exercise 3
+
+# New-school Roman numerals (4= IV, 9 = IX)
+
+# I = 1     V = 5     X = 10   L = 50
+# C = 100   D = 500   M = 1000
+# 400 = CD
+# 900 = CM
+
+
+
+def new_roman_numeral(number)
+  
+  roman = ""
+
+  thousands_place = number/1000
+  hundreds_place =  (number%1000)/100
+  tens_place = (number%100)/10
+  ones_place = (number%10)
+
+  roman << "M"*thousands_place
+
+  if hundreds_place == 9
+    roman << "CM"
+  elsif hundreds_place == 4
+    roman << "CD"
+  else
+     roman = roman << 'D' * (num%1000 /500)
+      roman = roman << 'C' * (num%500  /100)
+  end
+
+  if tens_place == 9
+    roman << "XC"
+  elsif tens_place == 4
+    roman << "XL"
+  else
+    roman = roman << 'L' * (num%100  /50)
+    roman = roman << 'X' * (num%50   /10)
+  end
+
+
+  if ones_place == 9
+    roman << "IX"
+  elsif ones_place == 4
+    roman << "IV"
+  else
+    roman = roman << 'V' * (num%10   /5)
+    roman = roman << 'I' * (num%5)
+  end
+    
+
+
+end
+puts "Please enter a number between 1 and 3000."
+num = gets.chomp.to_i
+
+puts new_roman_numeral(num)
+
+
+
+
