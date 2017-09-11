d38a3a89cbb5e9f62bd7cb473a29a1b22427c7a4
Sidharth Nambiar <snambiar01@gmail.com>
2017-09-11 18:25:51 -0400

    started practice problems easy 1

---
+++ course_101_programming_foundations/lesson_3/practice_problems_easy_2/ques_1.rb
@@ -0,0 +1,28 @@
+# 101 Programming Foundations
+# Lesson 3: Practice Problems
+# Practice Problems: Easy 1 - Question 1
+
+# In this hash of people and their age,
+
+ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 402, "Eddie" => 10 }
+
+#see if "Spot" is present.
+
+ages.has_key?("Spot") ? puts("Spot is present.") : puts("Spot is not present.")
+
+#Bonus: What are two other hash methods that would work just as well for this solution?
+
+# Alternate method 1
+
+ages.include?("Spot") ? puts("Spot is present.") : puts("Spot is not present.")
+
+# Alternate method 2
+
+ages.key?("Spot") ? puts("Spot is present.") : puts("Spot is not present.")
+
+
+# Alternate method 3
+
+ages.member?("Spot") ? puts("Spot is present.") : puts("Spot is not present.")
+
+
