bf795fc8b33df8f9f733f82f190df289abe9420d
Sidharth Nambiar <snambiar01@gmail.com>
2018-01-20 23:26:40 -0500

    101-109 small problems

---
+++ course_101_programming_foundations/101-109_small_problems/easy_4_ex_1.rb
@@ -0,0 +1,31 @@
+# Course 101 Programming Foundations
+# 101-109 Small Problems
+# Easy 4: Exercise 1: Short Long Short
+
+# Write a method that takes two strings as arguments, determines
+# the longest of the two strings, and then returns the result of 
+# concatenating the shorter string, the longer string, and the shorter
+# string, and the shorter stirng once again. You may assume that the strings 
+# are of different lengths
+
+
+
+def short_long_short(str1, str2)
+	if str1.length < str2.length
+		str1 + str2 + str1
+	else
+		str2 + str1 + str2
+	end
+	
+end
+
+
+
+
+# Examples:
+
+
+puts short_long_short('abc', 'defgh') == "abcdefghabc"
+puts short_long_short('abcde', 'fgh') == "fghabcdefgh"
+puts short_long_short('','xyz') == "xyz"
+
---
+++ course_101_programming_foundations/101-109_small_problems/easy_4_ex_2.rb
@@ -0,0 +1,31 @@
+# Course 101 Programming Foundations
+# 101-109 Small Problems
+# Easy 4: Exercise 2 - What Centrury is That?
+
+# Write a method that takes a year as input and returns the century.
+# The return value should be a string that begins with the century
+# number, and ends with st, nd, rd, or th as appropriate for that number.
+
+# New centuries begin in years that end with 01. So, the years 1901-2000 comprise the 
+# 20th centrury.
+
+
+
+
+
+
+
+# Examples:
+century(2000) == '20th'
+century(2001) == '21st'
+century(1965) == '20th'
+century(256) == '3rd'
+century(5) == '1st'
+century(10103) == '102nd'
+century(1052) == '11th'
+century(1127) == '12th'
+century(11201) == '113th'
+
+
+
+
