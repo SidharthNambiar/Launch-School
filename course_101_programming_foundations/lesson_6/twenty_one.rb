01226d19e0e6d6fc62506f946dd5556649b971f1
Sidharth Nambiar <snambiar01@gmail.com>
2018-03-06 11:14:15 -0500

    twenty_one passed rubocop

--- course_101_programming_foundations/lesson_6/twenty_one.rb
+++ course_101_programming_foundations/lesson_6/twenty_one.rb
@@ -1,63 +1,40 @@
 # Course 101 Programming Foundations
 # Lesson 6: Slightly Larger Programs
-# Walk-through: Twenty-One
-
-# Psuedo code:
-# 1. Initialize deck
-# 2. Deal cards to player and dealer
-# 3. Player turn: hit or stay
-#   - repeat until bust or "stay"
-# 4. If player bust, dealer wins.
-# 5. Dealer turn: hit or stay
-#   - repeat until total >= 17
-# 6. If dealer bust, player wins.
-# 7. Compare cards and declare winner.
-
-# Card Value
-# 2-10 = face value
-# jack, queen, king = 10
-# ace = 1 or 11
-
-# suits: hearts (h), diamonds (d), clubs (c), and spades (s)
-
-# deck = ['2h', '3h', '4h', '5h', '6h', '7h', '8h', '9h', '10h', 'jh', 'qh', 'kh', 'ah',
-#         '2d', '3d', '4d', '5d', '6d', '7d', '8d', '9d', '10d', 'jd', 'qd', 'kd', 'ad',
-#         '2c', '3c', '4c', '5c', '6c', '7c', '8c', '9c', '10c', 'jc', 'qc', 'kc', 'ac',
-#         '2s', '3s', '4s', '5s', '6s', '7s', '8s', '9s', '10s', 'js', 'qs', 'ks', 'as']
+# Assignment: Twenty-One
 
 require 'pry'
 
-SUITS = ['h', 'd', 'c', 's']  # heart, diamond, club, spade
-NON_FACE_VALUE_CARDS = ['j', 'q', 'k', 'a'] # jack, queen, king, ace
+SUITS = ['h', 'd', 'c', 's']
+NON_FACE_VALUE_CARDS = ['j', 'q', 'k', 'a']
 FACE_VALUE_CARDS = (2..10).to_a
 VALID_GAMEPLAY_RESPONSES = ['h', 's']
-NON_FACE_VALUE_CARDS_DEF = {'j' => 'Jack', 'q' => 'Queen', 'k' => 'King', 'a' => 'Ace'}
-
+VALID_PLAY_AGAIN_RESPONSES = ['y', 'n']
+NON_FACE_VALUE_CARDS_DEF = { 'j' => 'Jack', 'q' => 'Queen',
+                             'k' => 'King', 'a' => 'Ace' }
 
 def prompt(msg)
   puts "=> #{msg}"
 end
 
 def initialize_deck
-  deck = []  
+  deck = []
   SUITS.each do |suit|
-    FACE_VALUE_CARDS.each {|card| deck << (card.to_s + suit)}
-    NON_FACE_VALUE_CARDS.each {|card| deck << (card + suit)}
+    FACE_VALUE_CARDS.each { |card| deck << (card.to_s + suit) }
+    NON_FACE_VALUE_CARDS.each { |card| deck << (card + suit) }
   end
   deck
 end
 
 def determine_card_value(card, curr_deck_value)
   arr = card.chars
-  value = 0
   if NON_FACE_VALUE_CARDS.include?(arr.first) || arr.size > 2
     if arr.first == 'a'
-      value = determine_ace_value(curr_deck_value)
+      return determine_ace_value(curr_deck_value)
     else
-      value = 10
+      return 10
     end
   else
-    value = arr.first.to_i
+    return arr.first.to_i
   end
 end
 
@@ -70,14 +47,7 @@ def determine_deck_value(deck)
 end
 
 def determine_ace_value(curr_deck_value)
-  return_val = 0
-  if curr_deck_value < 11
-    return_val = 11
-  else
-    return_val = 1
-  end
-  return_val
-
+  curr_deck_value < 11 ? 11 : 1
 end
 
 def deal_card(deck, num)
@@ -92,8 +62,7 @@ end
 
 def determine_card_name(card)
   arr = card.chars
-
-  if NON_FACE_VALUE_CARDS_DEF.has_key?(arr.first)
+  if NON_FACE_VALUE_CARDS_DEF.key?(arr.first)
     NON_FACE_VALUE_CARDS_DEF[arr.first]
   elsif arr.size > 2
     10.to_s
@@ -105,42 +74,32 @@ end
 def get_deck_name(deck)
   deck_name = ""
   deck.each_with_index do |card, ind|
-    if ind == (deck.size-1 )
+    if ind == (deck.size - 1)
       deck_name << ' and' << " #{determine_card_name(card)}"
     else
-      deck_name << "#{determine_card_name(card)}"
+      deck_name << determine_card_name(card).to_s
     end
-    deck_name << ", " if (ind <= deck.size-3) && deck.size > 2
+    deck_name << ", " if (ind <= deck.size - 3) && deck.size > 2
   end
   deck_name
 end
 
-
-def display(player_deck, dealer_deck, player_deck_val, dealer_deck_val)
+def display(player_deck, dealer_deck, player_deck_val, _dealer_deck_val)
   system 'clear'
-  player_deck_name = ''
-  dealer_deck_name = ''
   dealer_deck_name = determine_card_name(dealer_deck.first)
   player_deck_name = get_deck_name(player_deck)
-
   prompt "Dealer has: #{dealer_deck_name} and unknown card(s)."
   prompt "You have: #{player_deck_name}."
   prompt "Player total value: #{player_deck_val}"
-  
 end
 
 def display_all(player_deck, dealer_deck, player_deck_val, dealer_deck_val)
-  #system 'clear'
-  player_deck_name = ''
-  dealer_deck_name = ''
   dealer_deck_name = get_deck_name(dealer_deck)
   player_deck_name = get_deck_name(player_deck)
-
-  prompt "Dealer has: #{dealer_deck_name}."
-  prompt "Dealer total value: #{dealer_deck_val}"
+  prompt "Dealer had: #{dealer_deck_name}. Total value: #{dealer_deck_val}"
   puts "\n"
-  prompt "You have: #{player_deck_name}."
-  prompt "Player total value: #{player_deck_val}"
+  prompt "You had: #{player_deck_name}. Total value: #{player_deck_val}"
+  puts "\n"
 end
 
 def determine_winner(player_total, dealer_total)
@@ -152,8 +111,15 @@ def determine_winner(player_total, dealer_total)
   nil
 end
 
+def display_winner(the_winner)
+  system 'clear'
+  puts "\n********************"
+  puts the_winner.nil? ? "It's a tie!" : "#{the_winner} won the game!"
+  puts "********************"
+end
+
 loop do
-  system 'clear' 
+  system 'clear'
   deck = initialize_deck
   player_deck = deal_card(deck, 2)
   dealer_deck = deal_card(deck, 2)
@@ -163,26 +129,21 @@ loop do
   dealer_deck_value = determine_deck_value(dealer_deck)
   player_move = ''
   winner = ''
+  answer = ''
 
   display(player_deck, dealer_deck, player_deck_value, dealer_deck_value)
 
   loop do
-    
-    # Deal player cards until player stays
-    while player_move != 's' do
-            
+    # ask player to hit or stay
+    while player_move != 's'
       loop do
-
         puts "\n"
         prompt "Do you want to 'hit' or 'stay'?"
         prompt "Enter 'h' to hit or 's' to stay. (case insensitive)"
         player_move = gets.chomp.downcase
         break if VALID_GAMEPLAY_RESPONSES.include?(player_move)
-        #display(player_deck, dealer_deck, player_deck_value, dealer_deck_value)
         prompt "Invalid Entry! Try again!"
       end
-
-
 
       if player_move == 'h'
         player_deck += deal_card(deck, 1)
@@ -190,40 +151,33 @@ loop do
         display(player_deck, dealer_deck, player_deck_value, dealer_deck_value)
         break if player_deck_value > 21
       end
+
     end
-    
     # check if player bust
     winner = 'Dealer' if player_deck_value > 21
     break if winner.empty? == false
-    
-    
     # Deal cards to player as long as deck value is less than 18
     while dealer_deck_value < 18
       dealer_deck += deal_card(deck, 1)
       dealer_deck_value = determine_deck_value(dealer_deck)
     end
-   
-   display(player_deck, dealer_deck, player_deck_value, dealer_deck_value)
-
+    display(player_deck, dealer_deck, player_deck_value, dealer_deck_value)
     # check if player bust
     winner = 'Player' if dealer_deck_value > 21
     break if winner.empty? == false
-
     winner = determine_winner(player_deck_value, dealer_deck_value)
     break
   end
-  
-  system 'clear'
-  display_all(player_deck, dealer_deck, player_deck_value, dealer_deck_value)
-  puts "\n"
 
-  puts winner == nil ? "It's a tie!" : "#{winner} won the game!"
+  display_winner(winner)
+  display_all(player_deck, dealer_deck, player_deck_value, dealer_deck_value) 
 
-  
-
-
- 
-  prompt "Do you want to play again? (Enter 'y' or 'n')"
-  answer = gets.chomp
+  loop do
+    prompt "Do you want to play again?"
+    prompt "Hit 'y' to play again or 'n' to exit the game. (case insensitive)"
+    answer = gets.chomp
+    break if VALID_PLAY_AGAIN_RESPONSES.include?(answer.downcase)
+    prompt "Invalid entry!"
+  end
   break unless answer.downcase.start_with?("y")
 end
---
+++ course_101_programming_foundations/lesson_6/twenty_one_ls_version.rb
@@ -0,0 +1,3 @@
+# Course 101 Programming Foundations
+# Lesson 6: Slightly Larger Programs
+# Possible Solution: Twenty-One (Launch School's solution)
